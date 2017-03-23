afbb2d9148cf3ad303d068ededcdc8941469c9b1
Phayao Boonon <phayaob@gmail.com>
2017-03-24 06:42:23 +0700

    Update RunAsynce method

--- Program.cs
+++ Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace klongsambot
 {
     class Program
     {
+        static HttpClient client = new HttpClient();
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {            
+            RunAsync().Wait();
         }
+
+        static async Task RunAsync()
+        {
+            client.BaseAddress = new Uri("https://www.agoda.com/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
     }
 }
