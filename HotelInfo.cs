7a43f2d2adc5dc6c5ac7f8aea41d5c41e1d6edbb
Phayao Boonon <phayaob@gmail.com>
2017-03-26 19:50:29 +0700

    Get hotel information of city list

---
+++ HotelInfo.cs
@@ -0,0 +1,13 @@
+namespace klongsambot
+{
+    internal class HotelInfo
+    {
+        internal string ID { get; set; }
+        internal string RoomID { get; set; }
+        internal string Name { get; set; }
+        internal string StarRate { get; set; }
+        internal string AreaCity { get; set; }
+        internal string HighPrice { get; set; }
+        internal string LowPrice { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace klongsambot
@@ -11,29 +13,50 @@ namespace klongsambot
         static HttpClient client = new HttpClient();
         static void Main(string[] args)
         {            
-            SearchItem searchItem = GenSearchItem();
-            RunAsync(searchItem).Wait();
-            
+            RunAsync().Wait();
         }
 
-        static async Task RunAsync(SearchItem item) {
+        static async Task RunAsync() {
             client.BaseAddress = new Uri("https://www.agoda.com/");
             client.DefaultRequestHeaders.Accept.Clear();
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-            string testfile = "test.htm";
+            SearchItem item;
+            string[] cities = {"4064", "9395", "5085"};
+            foreach(string city in cities) {
+                item = GenSearchItem(city);
+                string resultHTML = string.Format("{0}\\{1}.htm", Directory.GetCurrentDirectory(), city);
+                string resultTxet = string.Format("{0}\\{1}.txt", Directory.GetCurrentDirectory(), city);
 
-            if(File.Exists(testfile)) 
-            {
-                File.Delete(testfile);
+                if (File.Exists(resultHTML))
+                    File.Delete(resultHTML);
+                
+                if (File.Exists(resultTxet))
+                    File.Delete(resultTxet);
+
+                HttpResponseMessage response = await client.GetAsync(string.Format("pages/agoda/default/DestinationSearchResult.aspx?{0}", GenUriParameter(item)));
+                if (response.IsSuccessStatusCode)
+                {
+                    string resString = await response.Content.ReadAsStringAsync();
+                    File.AppendAllText(resultHTML, resString);
+
+                    List<HotelInfo> hotelsInfo = GetHotelInfo(resString);
+                    foreach (HotelInfo hotelInfo in hotelsInfo) {
+                        string result = string.Empty;
+                        result += string.Format("ID:        {0}\n", hotelInfo.ID);
+                        result += string.Format("RoomID:    {0}\n", hotelInfo.RoomID);
+                        result += string.Format("Name:      {0}\n", hotelInfo.Name);
+                        result += string.Format("StarRate:  {0}\n", hotelInfo.StarRate);
+                        result += string.Format("AreaCity:  {0}\n", hotelInfo.AreaCity);
+                        result += string.Format("HighPrice: {0}\n", hotelInfo.HighPrice);
+                        result += "\n";
+                        File.AppendAllText(resultTxet, result);
+                    }
+
+                }         
             }
 
-            HttpResponseMessage response = await client.GetAsync(string.Format("pages/agoda/default/DestinationSearchResult.aspx?{0}", GenUriParameter(item)));
-            if(response.IsSuccessStatusCode)
-            {
-                string x = await response.Content.ReadAsStringAsync();
-                File.AppendAllText("test.htm", x);
-            }
+
         }
 
         static private SearchItem GenSearchItem() {
@@ -53,6 +76,12 @@ namespace klongsambot
             item.TrafficType = "User";
             item.CultureInfoName = "en-US";
 
+            return item;
+        }
+
+        static private SearchItem GenSearchItem(string cityID) {
+            SearchItem item = GenSearchItem();
+            item.CityID = cityID;
             return item;
         }
 
@@ -76,18 +105,143 @@ namespace klongsambot
             return parameter;
         }
 
+        static private List<HotelInfo> GetHotelInfo(string data) {
+            List<HotelInfo> hotelsInfo = new List<HotelInfo>();
 
-        // static async Task<string> GetAsync()
-        // {
-        //     string ret = string.Empty;
+            string tagHotelItem = "data-selenium=\"hotel-item\"";
+            string tagHotelId = "data-hotelid";
+            string tagRoomId = "data-roomid";
+            string tagPrice = "price soft-red";
+            string tagHighPrice = "price dark-gray1";
+            string tagHotelName = "hotel-name";
+            string tagAreaCity = "areacity-name-text";
+            string tagStarRate = "ficon ficon-star-";
 
-        //     HttpResponseMessage response = await client.GetAsync();
-        //     if(response.IsSuccessStatusCode)
-        //     {
-        //         ret = await response.Content.ReadAsStringAsync();
-        //     }
-        //     return ret;
-        // }
+            int idxHotelItem = 0;
+
+            try {
+                while(true) {
+                    idxHotelItem = data.IndexOf(tagHotelItem, idxHotelItem);
+
+                    if(idxHotelItem == -1) {
+                        break;
+                    }
+                    else {
+                        HotelInfo hotelInfo = new HotelInfo();
+                        int idx = idxHotelItem;
+                        // Looking for data-hotelid
+                        hotelInfo.ID = GetAttValue(tagHotelId, data, idx);
+                        // Looking for data-roomid
+                        hotelInfo.RoomID = GetAttValue(tagRoomId, data, idx);
+                        // Looking for hotelname
+                        hotelInfo.Name = GetElmValue(tagHotelName, data, idx);
+                        // Looking for areacity
+                        hotelInfo.AreaCity = GetElmValue(tagAreaCity, data, idx);
+                        // Looking for price
+                        hotelInfo.LowPrice = GetElmValue(tagPrice, data, idx);
+                        // Looking for high price
+                        hotelInfo.HighPrice = GetElmValue(tagHighPrice, data, idx);
+                        // Looking for starrate
+                        hotelInfo.StarRate = GetElmValueStar(tagStarRate, data, idx);
+
+                        if(!string.IsNullOrEmpty(hotelInfo.AreaCity)) {
+                            hotelsInfo.Add(hotelInfo);
+                        }
+                        
+                        idxHotelItem++;
+                    }
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            return hotelsInfo;
+        }
+
+        static string GetAttValue(string tagelement, string data, int idx) {
+            string value = string.Empty;
+            int loc = 0;
+
+            loc = data.IndexOf(tagelement, idx);
+            if(loc != -1)
+                value = GetAttValue(data, loc);
+
+            return value;
+        }
+
+        static string GetAttValue(string data, int idx) {
+            int attStrt = 0; 
+            int attEnd  = 0;
+            string tagAtt = "\"";
+            string value = string.Empty;
+            attStrt = data.IndexOf(tagAtt, idx) + 1;
+            attEnd = data.IndexOf(tagAtt, attStrt);
+            
+            value = data.Substring(attStrt, attEnd - attStrt);
+
+            return value;
+        }
+
+        static string GetElmValue(string tagclass, string data, int idx) {
+            string value = string.Empty;
+            int loc = 0;
+
+            loc = data.IndexOf(string.Format("class=\"{0}\"", tagclass), idx);
+            if(loc != -1)
+                value = GetElmValue(data, loc);
+
+            return value;
+        }
+
+        static string GetElmValue(string data, int idx) {
+            int elmStr = 0;
+            int elmEnd  = 0;
+
+            string tagElmStrOpen = "<";
+            string tagElmStrClose = "</";
+            string tagElmEd = ">";
+            string tagSpace = " ";
+
+            string element = string.Empty;
+            string value = string.Empty;
+
+            // get HTML element name
+            elmStr = data.Substring(0, idx).LastIndexOf(tagElmStrOpen) + 1;
+            elmEnd = data.IndexOf(tagSpace, elmStr);
+            element = data.Substring(elmStr, elmEnd - elmStr);
+
+            elmStr = data.IndexOf(tagElmEd, idx) + 1;
+            elmEnd = data.IndexOf(string.Format("{0}{1}", tagElmStrClose, element), elmStr);
+
+            value = Regex.Replace(data.Substring(elmStr, elmEnd - elmStr), @"\t|\n|\r", "");
+            if (value.Contains(tagElmStrOpen)) {
+                value = value.Substring(0, value.IndexOf(tagElmStrOpen));
+            }
+
+            return value.Trim();
+        }
+
+        static string GetElmValueStar(string tagclass, string data, int idx) {
+            string value = string.Empty;
+            string tagSpace = " ";
+
+            int loc = 0;
+            int spc = 0;
+
+            loc = data.IndexOf(string.Format("class=\"{0}", tagclass), idx) + tagclass.Length + 7;
+            if(loc != -1) {
+                spc = data.IndexOf(tagSpace, loc);
+                value = data.Substring(loc, spc - loc);
+                int star;
+                if(int.TryParse(value, out star)) {
+                    value = (star > 10) ? string.Format("{0}.{1}", (star / 10), (star % 10)) : value;
+                }
+            }
+                
+            return value;
+        }
 
     }
 }
